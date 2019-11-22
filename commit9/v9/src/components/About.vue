4ac270591de3342325abd3feb50412aca8bce0cd
mb2166 <58080841+mb2166@users.noreply.github.com>
2019-11-22 16:43:27 +0100

    Add files via upload

---
+++ commit9/v9/src/components/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is an App using VUE.JS</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About' //this is the name of the component
+}
+</script>
+<style>
+</style>
